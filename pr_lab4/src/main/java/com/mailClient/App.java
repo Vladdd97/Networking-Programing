1de40289261d37a9fc1ed3f58723abc4f9942de9
Vladdd97 <vladbantus@mail.ru>
2019-03-24 22:26:44 +0200

    refactored pr_lab4

--- pr_lab4/src/main/java/com/mailClient/App.java
+++ pr_lab4/src/main/java/com/mailClient/App.java
@@ -4,36 +4,69 @@ import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import java.util.Properties;
+import java.util.Scanner;
 
 /**
  * Hello world!
  */
 public class App {
 
-    public static final String USERNAME = "***";
-    public static final String PASSWORD = "***";
+    public static final String USERNAME = "vladbantus02@gmail.com";
+    public static final String PASSWORD = "ercvvpgjqjeyjius";
     public static final String TO = "vladislav.bantus@faf.utm.md";
 
     public static void main(String[] args) {
-//        System.out.println( "Hello World!" );
-//        try {
-//            ReadEmail.getEmails();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
 
-        //SSLEmail.sendEmailViaSSl();
 
-        Session session = Session.getDefaultInstance(getPOP3Properties(), createAuthenticatorObject());
-        System.out.println("Session created");
-        try {
-            EmailUtil.readEmails(session,USERNAME,PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
+        System.out.println("\n" +
+                "Enter 1 for reading messages" +
+                "\nEnter 2 for sending message" +
+                "\n\n\n\tYour choice: ");
+        Scanner in = new Scanner(System.in);
+        int choice = in.nextInt();
+
+
+        executeOption(choice);
+
+    }
+
+    public static void executeOption(int choice){
+
+        Session session;
+        switch (choice) {
+            case 1:
+
+                // 3. Creating mail session.
+                session = Session.getDefaultInstance(getPOP3Properties(), createAuthenticatorObject());
+                try {
+                    EmailUtil.readEmails(session, USERNAME, PASSWORD);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case 2:
+                // 3. Creating mail session.
+                session = Session.getDefaultInstance(getSMTPProperties(), createAuthenticatorObject());
+                Scanner in = new Scanner(System.in);
+
+                System.out.println("Enter the subject:");
+                String subject = in.nextLine();
+
+                System.out.println("Enter the content:");
+                String content = in.nextLine();
+                EmailUtil.sendImageEmail(session, TO, subject, content,
+                        "D:\\Programs\\_about\\selfDevelopment\\pr_lab4\\src\\main\\resources\\images\\image.jpg");
+                break;
+
+            default:
+                System.out.println("wrong input");
         }
+
     }
 
     public static Properties getPOP3Properties() {
+        // 1. Setup properties for the mail session.
         Properties props = new Properties();
         props.put("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.pop3.socketFactory.fallback", "false");
@@ -46,7 +79,8 @@ public class App {
         return props;
     }
 
-    public static Properties getSMTPProperties(){
+    public static Properties getSMTPProperties() {
+        // 1. Setup properties for the mail session.
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
         props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
@@ -60,6 +94,7 @@ public class App {
 
     public static Authenticator createAuthenticatorObject() {
 
+        // 2. Creates a javax.mail.Authenticator object.
         Authenticator auth = new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
--- pr_lab4/src/main/java/com/mailClient/EmailUtil.java
+++ pr_lab4/src/main/java/com/mailClient/EmailUtil.java
@@ -1,14 +1,24 @@
 package com.mailClient;
 
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class EmailUtil {
+
+    private static final String REPLY = "vladbantus02@gmail.com";
+    private static final String PERSONAL = "Bantus Vladislav";
 
     public static void sendEmail(Session session, String toEmail, String subject, String body){
         try
@@ -19,9 +29,9 @@ public class EmailUtil {
             msg.addHeader("format", "flowed");
             msg.addHeader("Content-Transfer-Encoding", "8bit");
 
-            msg.setFrom(new InternetAddress("vladbantus02@gmail.com", "Bantus Vladislav"));
+            msg.setFrom(new InternetAddress(REPLY, PERSONAL));
 
-            msg.setReplyTo(InternetAddress.parse("vladbantus02@gmail.com", false));
+            msg.setReplyTo(InternetAddress.parse(REPLY, false));
 
             msg.setSubject(subject, "UTF-8");
 
@@ -66,20 +76,17 @@ public class EmailUtil {
                     .getRecipients(Message.RecipientType.CC));
             String sentDate = message.getSentDate().toString();
 
-            //String contentType = message.getContentType();
 
-            Multipart mp = (Multipart) message.getContent();
-            Part part = mp.getBodyPart(0);
-            String messageContent = part.getContent().toString();
-
+            String messageContent = getTextFromMimeMultipart((MimeMultipart) message.getContent());
             // print out details of each message
-            System.out.println("\n-----------------------");
+            System.out.println("\n==============================");
             System.out.println("\t From: " + from);
             System.out.println("\t To: " + toList);
             System.out.println("\t CC: " + ccList);
             System.out.println("\t Subject: " + subject);
             System.out.println("\t Sent Date: " + sentDate);
             System.out.println("\t Message: " + messageContent);
+            System.out.println("\n==============================\n");
 
         }
 
@@ -87,6 +94,26 @@ public class EmailUtil {
         // 7. Close folder and close store.
         inbox.close(false);
         store.close();
+    }
+
+
+    private static String getTextFromMimeMultipart(
+            MimeMultipart mimeMultipart)  throws MessagingException, IOException {
+        String result = "";
+        int count = mimeMultipart.getCount();
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                result = result + "\n" + bodyPart.getContent();
+                break; // without break same text appears twice in my tests
+            } else if (bodyPart.isMimeType("text/html")) {
+                String html = (String) bodyPart.getContent();
+                result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
+            } else if (bodyPart.getContent() instanceof MimeMultipart){
+                result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+            }
+        }
+        return result;
     }
 
     public static String parseAddresses(Address[] address) {
@@ -106,7 +133,63 @@ public class EmailUtil {
 
 
 
+    public static void sendImageEmail(Session session, String toEmail, String subject, String body, String filename){
+        try{
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
 
+            try {
+                msg.setFrom(new InternetAddress(REPLY, PERSONAL));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            msg.setReplyTo(InternetAddress.parse(REPLY, false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            messageBodyPart.setText(body);
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is image attachment
+            messageBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            //Trick is to add the content-id header here
+            messageBodyPart.setHeader("Content-ID", "image_id");
+            multipart.addBodyPart(messageBodyPart);
+
+            //third part for displaying image in the email body
+            messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent("<h1>Attached Image</h1>" +
+                    "<img src='cid:image_id'>", "text/html");
+            multipart.addBodyPart(messageBodyPart);
+
+            //Set the multipart message to the email message
+            msg.setContent(multipart);
+
+            // Send message
+            Transport.send(msg);
+            System.out.println("EMail Sent Successfully with image!!");
+        }catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
